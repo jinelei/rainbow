e8a2367c567dbd5ff4357998bc000c6decb3252b
jinelei <jinelei@163.com>
2018-12-12 17:29:45 +0800

    添加netty模块

---
+++ server/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module rainbow.server {
+    requires transitive io.netty.all;
+    exports rainbow.server;
+}
---
+++ server/src/main/java/rainbow/server/DiscardServer.java
@@ -0,0 +1,17 @@
+package rainbow.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author zhenlei
+ */
+public class DiscardServer extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        buf.release();
+    }
+
+}
