f2255bd148fb60f0a6a56309b59fe98c9db9ef44
jinelei <jinelei@163.com>
2019-01-17 15:54:52 +0800

    添加错误码返回处理

--- blog/src/main/java/cn/jinelei/rainbow/blog/exception/handler/GlobalExceptionhandler.java
+++ blog/src/main/java/cn/jinelei/rainbow/blog/exception/handler/GlobalExceptionhandler.java
@@ -16,7 +16,6 @@ public class GlobalExceptionhandler {
     @ExceptionHandler(value = {CustomizeException.class})
     @ResponseBody
     public ErrorView handleException(CustomizeException e) {
-        System.out.println(e.toString());
         return new ErrorView(e);
     }
 }
--- blog/src/main/java/cn/jinelei/rainbow/blog/view/ErrorView.java
+++ blog/src/main/java/cn/jinelei/rainbow/blog/view/ErrorView.java
@@ -32,4 +32,20 @@ public class ErrorView implements Serializable {
                 ", message='" + message + '\'' +
                 '}';
     }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
